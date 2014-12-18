e29ffb2d3ab8f018d029f1a50f5457401b9e5228
Daniel Smith <daniel@smitbo.com>
2014-12-17 21:53:19 -0500

    Changed pin assignments in software.  Added support for use of DIP switches to control timing for different Heat sinks

--- src/circuitpins.h
+++ src/circuitpins.h
@@ -9,123 +9,161 @@
 #define CIRCUITPINS_H_
 
 /*
- * LED 2 ---> PORTA_2
- * PUSH1 ---> PINA_1
- * LED 3 ---> PORTA_0
- * LED 4 ---> PORTB_7
- * LED 5 ---> PORTB_6
+ * LED 1 ---> PORTB_0
+ * LED 2 ---> PORTB_1
+ * LED 3 ---> PORTB_2
+ * LED 4 ---> PORTB_3
+ * LED 5 ---> PORTB_4
  * LED 6 ---> PORTB_5
- * LED 7 ---> PORTD_6
- * LED 8 ---> PORTB_4
- * LED 9 ---> PORTD_5
- * LED10 ---> PORTB_3
- * LED11 ---> PORTD_4
- * LED12 ---> PORTB_2
- * LED13 ---> PORTD_3
- * LED14 ---> PORTB_1
- * LED15 ---> PORTD_2
- * LED16 ---> PORTB_0
- * LED17 ---> PORTD_1
- * LED18 ---> PORTD_0
+ * LED 7 ---> PORTB_6
+ * LED 8 ---> PORTB_7
+ * DIP1  ---> PINC_0
+ * DIP2  ---> PINC_1
+ * DIP3  ---> PINC_2
+ * DIP4  ---> PINC_3
+ * NC    ---> PINC_4
+ * NC    ---> PINC_5
+ * LED 9 ---> PORTD_0
+ * LED10 ---> PORTD_1
+ * LED11 ---> PORTD_2
+ * LED12 ---> PORTD_3
+ * LED13 ---> PORTD_4
+ * LED14 ---> PORTD_5
+ * LED15 ---> PORTD_6
+ * LED16 ---> PORTD_7
  */
 // Values for DDRx Registers
-#define DIRECTION_A 0x07
+#define DIRECTION_C 0x00
 #define DIRECTION_B 0xFF
-#define DIRECTION_D 0x7F
+#define DIRECTION_D 0xFF
 
 // At Time 00
-#define BANK_A_00 0x00
 #define BANK_B_00 0x00
-#define BANK_D_00 0x01
+#define BANK_D_00 0x00
 
 // At Time 01
-#define BANK_A_01 0x02
-#define BANK_B_01 0x00
-#define BANK_D_01 0x01
+#define BANK_B_01 0x01
+#define BANK_D_01 0x00
 
 // At Time 02
-#define BANK_A_02 0x03
-#define BANK_B_02 0x00
-#define BANK_D_02 0x01
+#define BANK_B_02 0x03
+#define BANK_D_02 0x00
 
 // At Time 03
-#define BANK_A_03 0x07
-#define BANK_B_03 0x80
-#define BANK_D_03 0x01
+#define BANK_B_03 0x07
+#define BANK_D_03 0x00
 
 // At Time 04
-#define BANK_A_04 0x07
-#define BANK_B_04 0xC0
-#define BANK_D_04 0x01
+#define BANK_B_04 0x0F
+#define BANK_D_04 0x00
 
 // At Time 05
-#define BANK_A_05 0x07
-#define BANK_B_05 0xE0
-#define BANK_D_05 0x01
+#define BANK_B_05 0x1F
+#define BANK_D_05 0x00
 
 // At Time 06
-#define BANK_A_06 0x07
-#define BANK_B_06 0xE0
-#define BANK_D_06 0x41
+#define BANK_B_06 0x3F
+#define BANK_D_06 0x00
 
 // At Time 07
-#define BANK_A_07 0x07
-#define BANK_B_07 0xF0
-#define BANK_D_07 0x41
+#define BANK_B_07 0x7F
+#define BANK_D_07 0x00
 
 // At Time 08
-#define BANK_A_08 0x07
-#define BANK_B_08 0xF0
-#define BANK_D_08 0x61
+#define BANK_B_08 0xFF
+#define BANK_D_08 0x00
 
 // At Time 09
-#define BANK_A_09 0x07
-#define BANK_B_09 0xF8
-#define BANK_D_09 0x61
+#define BANK_B_09 0xFF
+#define BANK_D_09 0x01
 
 // At Time 10
-#define BANK_A_10 0x07
-#define BANK_B_10 0xF8
-#define BANK_D_10 0x71
+#define BANK_B_10 0xFF
+#define BANK_D_10 0x03
 
 // At Time 11
-#define BANK_A_11 0x07
-#define BANK_B_11 0xFC
-#define BANK_D_11 0x71
+#define BANK_B_11 0xFF
+#define BANK_D_11 0x07
 
 // At Time 12
-#define BANK_A_12 0x07
-#define BANK_B_12 0xFC
-#define BANK_D_12 0x79
+#define BANK_B_12 0xFF
+#define BANK_D_12 0x0F
 
 // At Time 13
-#define BANK_A_13 0x07
-#define BANK_B_13 0xFE
-#define BANK_D_13 0x79
+#define BANK_B_13 0xFF
+#define BANK_D_13 0x1F
 
 // At Time 14
-#define BANK_A_14 0x07
-#define BANK_B_14 0xFE
-#define BANK_D_14 0x7D
+#define BANK_B_14 0xFF
+#define BANK_D_14 0x3F
 
 // At Time 15
-#define BANK_A_15 0x07
 #define BANK_B_15 0xFF
-#define BANK_D_15 0x7D
+#define BANK_D_15 0x7F
 
 // At Time 16
-#define BANK_A_16 0x07
 #define BANK_B_16 0xFF
-#define BANK_D_16 0x7E
+#define BANK_D_16 0xFF
 
 // Empty Rest
-#define BANK_A_RESTE 0x02
 #define BANK_B_RESTE 0x00
 #define BANK_D_RESTE 0x00
 
 // PRESETS
 
 #define CLOCK_SELECT 0x05
-#define INPUT_MASK 0x02
+#define HS1_MASK 0x03
+#define HS2_MASK 0x0C
+
+// DEFAULT TIMING SETTINGS:
+#define BLINKSDEF 10
+#define TIMEONDEF 0x1F40  // 0x1F40 translates to 1 second on
+#define TIMEOFFDEF 0x1B58 // 0x1B58 translates to about 7/8ths of a second off
+
+// Timing Alterations
+	//CHS, 0
+	#define BLINKS10 8
+	#define TIMEON10 TIMEONDEF
+	#define TIMEOFF10 TIMEOFFDEF
+
+	//CHS, CHS
+	#define BLINKS11 9
+	#define TIMEON11 0x1B58
+	#define TIMEOFF11 0x1388
+
+	//RHS, 0
+	#define BLINKS20 5
+	#define TIMEON20 TIMEONDEF
+	#define TIMEOFF20 TIMEOFFDEF
+
+	//RHS, CHS
+	#define BLINKS21 6
+	#define TIMEON21 TIMEOFFDEF
+	#define TIMEOFF21 0x109A
+
+	//RHS, RHS
+	#define BLINKS22 5
+	#define TIMEON22 0x1770
+	#define TIMEOFF22 0x1482
+
+	//VRHS, 0
+	#define BLINKS30 4
+	#define TIMEON30 TIMEON22
+	#define TIMEOFF30 TIMEOFF22
+
+	//VRHS, CHS
+	#define BLINKS31 3
+	#define TIMEON31 TIMEONDEF
+	#define TIMEOFF31 0x157C
+
+	//VRHS, RHS
+	#define BLINKS32 3
+	#define TIMEON32 TIMEON22
+	#define TIMEOFF32 TIMEOFF22
+
+	//VRHS, VRHS
+	#define BLINKS33 2
+	#define TIMEON33 TIMEONDEF
+	#define TIMEOFF33 0x1996
 
 #endif /* CIRCUITPINS_H_ */
--- src/main.c
+++ src/main.c
@@ -9,205 +9,224 @@
 #include <stdlib.h>
 #include <avr/io.h>
 #include "circuitpins.h"
+
 void setup();
 
-void setPorts(int PA, int PB, int PD);
+void setPorts(int PB, int PD);
+
+int hsSettings();
 
 int main() {
 	setup();
+	int blinks;
+	long int timeon, timeoff;
+	blinks = BLINKSDEF;
+	timeon = TIMEONDEF;
+	timeoff = TIMEOFFDEF;
+	int hsset = hsSettings();
+	switch(hsset) {
+	case 4:
+		blinks = BLINKS10;
+		timeon = TIMEON10;
+		timeoff = TIMEOFF10;
+		break;
+	case 5:
+		blinks = BLINKS11;
+		timeon = TIMEON11;
+		timeoff = TIMEOFF11;
+		break;
+	case 8:
+		blinks = BLINKS20;
+		timeon = TIMEON20;
+		timeoff = TIMEOFF20;
+		break;
+	case 9:
+		blinks = BLINKS21;
+		timeon = TIMEON21;
+		timeoff = TIMEOFF21;
+		break;
+	case 10:
+		blinks = BLINKS22;
+		timeon = TIMEON22;
+		timeoff = TIMEOFF22;
+		break;
+	case 12:
+		blinks = BLINKS30;
+		timeon = TIMEON30;
+		timeoff = TIMEOFF30;
+		break;
+	case 13:
+		blinks = BLINKS31;
+		timeon = TIMEON31;
+		timeoff = TIMEOFF31;
+		break;
+	case 14:
+		blinks = BLINKS32;
+		timeon = TIMEON32;
+		timeoff = TIMEOFF32;
+		break;
+	case 15:
+		blinks = BLINKS33;
+		timeon = TIMEON33;
+		timeoff = TIMEOFF33;
+	}
 	int level = 1;
-	int PA, PB, PD;
+	int PB, PD;
 	while(level <= 16) {
-		for(int i = 0; i < 10; i++) {
+		for(int i = 0; i < blinks; i++) {
 			switch(level - 1) {
 					case 0:
-						PA = BANK_A_00;
 						PB = BANK_B_00;
 						PD = BANK_D_00;
 					break;
 					case 1:
-						PA = BANK_A_01;
 						PB = BANK_B_01;
 						PD = BANK_D_01;
 					break;
 					case 2:
-						PA = BANK_A_02;
 						PB = BANK_B_02;
 						PD = BANK_D_02;
 					break;
 					case 3:
-						PA = BANK_A_03;
 						PB = BANK_B_03;
 						PD = BANK_D_03;
 					break;
 					case 4:
-						PA = BANK_A_04;
 						PB = BANK_B_04;
 						PD = BANK_D_04;
 					break;
 					case 5:
-						PA = BANK_A_05;
 						PB = BANK_B_05;
 						PD = BANK_D_05;
 					break;
 					case 6:
-						PA = BANK_A_06;
 						PB = BANK_B_06;
 						PD = BANK_D_06;
 					break;
 					case 7:
-						PA = BANK_A_07;
 						PB = BANK_B_07;
 						PD = BANK_D_07;
 					break;
 					case 8:
-						PA = BANK_A_08;
 						PB = BANK_B_08;
 						PD = BANK_D_08;
 					break;
 					case 9:
-						PA = BANK_A_09;
 						PB = BANK_B_09;
 						PD = BANK_D_09;
 					break;
 					case 10:
-						PA = BANK_A_10;
 						PB = BANK_B_10;
 						PD = BANK_D_10;
 					break;
 					case 11:
-						PA = BANK_A_11;
 						PB = BANK_B_11;
 						PD = BANK_D_11;
 					break;
 					case 12:
-						PA = BANK_A_12;
 						PB = BANK_B_12;
 						PD = BANK_D_12;
 					break;
 					case 13:
-						PA = BANK_A_13;
 						PB = BANK_B_13;
 						PD = BANK_D_13;
 					break;
 					case 14:
-						PA = BANK_A_14;
 						PB = BANK_B_14;
 						PD = BANK_D_14;
 					break;
 					case 15:
-						PA = BANK_A_15;
 						PB = BANK_B_15;
 						PD = BANK_D_15;
 					break;
 					case 16:
-						PA = BANK_A_16;
 						PB = BANK_B_16;
 						PD = BANK_D_16;
 					break;
 					default:
-						PA = BANK_A_16;
 						PB = BANK_B_16;
 						PD = BANK_D_16;
 				}
-				setPorts(PA, PB, PD);
+				setPorts(PB, PD);
 				TCNT1 = 0x0000;
-				while(TCNT1 < 0x1F40) { // 0x1F40 translates to 1 second on
+				while(TCNT1 < timeon) {
 				}
 				switch(level) {
 						case 0:
-							PA = BANK_A_00;
 							PB = BANK_B_00;
 							PD = BANK_D_00;
 						break;
 						case 1:
-							PA = BANK_A_01;
 							PB = BANK_B_01;
 							PD = BANK_D_01;
 						break;
 						case 2:
-							PA = BANK_A_02;
 							PB = BANK_B_02;
 							PD = BANK_D_02;
 						break;
 						case 3:
-							PA = BANK_A_03;
 							PB = BANK_B_03;
 							PD = BANK_D_03;
 						break;
 						case 4:
-							PA = BANK_A_04;
 							PB = BANK_B_04;
 							PD = BANK_D_04;
 						break;
 						case 5:
-							PA = BANK_A_05;
 							PB = BANK_B_05;
 							PD = BANK_D_05;
 						break;
 						case 6:
-							PA = BANK_A_06;
 							PB = BANK_B_06;
 							PD = BANK_D_06;
 						break;
 						case 7:
-							PA = BANK_A_07;
 							PB = BANK_B_07;
 							PD = BANK_D_07;
 						break;
 						case 8:
-							PA = BANK_A_08;
 							PB = BANK_B_08;
 							PD = BANK_D_08;
 						break;
 						case 9:
-							PA = BANK_A_09;
 							PB = BANK_B_09;
 							PD = BANK_D_09;
 						break;
 						case 10:
-							PA = BANK_A_10;
 							PB = BANK_B_10;
 							PD = BANK_D_10;
 						break;
 						case 11:
-							PA = BANK_A_11;
 							PB = BANK_B_11;
 							PD = BANK_D_11;
 						break;
 						case 12:
-							PA = BANK_A_12;
 							PB = BANK_B_12;
 							PD = BANK_D_12;
 						break;
 						case 13:
-							PA = BANK_A_13;
 							PB = BANK_B_13;
 							PD = BANK_D_13;
 						break;
 						case 14:
-							PA = BANK_A_14;
 							PB = BANK_B_14;
 							PD = BANK_D_14;
 						break;
 						case 15:
-							PA = BANK_A_15;
 							PB = BANK_B_15;
 							PD = BANK_D_15;
 						break;
 						case 16:
-							PA = BANK_A_16;
 							PB = BANK_B_16;
 							PD = BANK_D_16;
 						break;
 						default:
-							PA = BANK_A_16;
 							PB = BANK_B_16;
 							PD = BANK_D_16;
 					}
-					setPorts(PA, PB, PD);
+					setPorts(PB, PD);
 					TCNT1 = 0x0000;
-					while(TCNT1 < 0x1B58) { // 0x1B58 translates to about 7/8ths of a second off
+					while(TCNT1 < timeoff) {
 					}
 		}
 	level++;
@@ -217,15 +236,55 @@ int main() {
 }
 
 void setup() {
-	DDRA = DIRECTION_A;
+	DDRC = DIRECTION_C;
 	DDRB = DIRECTION_B;
 	DDRD = DIRECTION_D;
-	setPorts(BANK_A_RESTE, BANK_B_RESTE, BANK_D_RESTE);
+	setPorts(BANK_B_RESTE, BANK_D_RESTE);
 	TCCR1B = CLOCK_SELECT;
 }
 
-void setPorts(int PA, int PB, int PD) {
-	PORTA = PA;
+void setPorts(int PB, int PD) {
 	PORTB = PB;
 	PORTD = PD;
+}
+
+int hsSettings() {
+	int HS1 = PINC & HS1_MASK;
+	int HS2 = (PINC & HS2_MASK) >> 2;
+	switch(HS1) {
+	case 0:
+		HS1 = 0;
+		break;
+	case 1:
+		HS1 = 2;
+		break;
+	case 2:
+		HS1 = 1;
+		break;
+	case 3:
+		HS1 = 3;
+	}
+	switch(HS2) {
+	case 0:
+		HS2 = 0;
+		break;
+	case 1:
+		HS2 = 2;
+		break;
+	case 2:
+		HS2 = 1;
+		break;
+	case 3:
+		HS2 = 3;
+	}
+	int HHS, HS;
+	if(HS1 > HS2) {
+		HHS = HS1;
+		HS = HS2;
+	}
+	else {
+		HHS = HS2;
+		HS = HS1;
+	}
+	return (HHS << 2) | (HS);
 }
